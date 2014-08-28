1a3642f2b974a3ece3e7ce2af8072025a4461478
Ashley <a.clarkem@gmail.com>
2014-08-28 09:56:09 -0600

    shops_without_favorite working

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -2,5 +2,15 @@ class Buyer < ActiveRecord::Base
 
   has_many :bikes
 
+  def shops_without_favorite
+    bike = Bike.find_by(brand: self.favorite_brand)
+    shop = Shop.find_by(id: bike.shop_id)
+    shops = Shop.where.not(id: shop.id)
+    name = []
+    shops.each do |shop_name|
+      name.push(shop_name.name)
+    end
+    name
+  end
 
 end
