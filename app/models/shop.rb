adfc4efa8ce4d6b0c03f43726449607008d23f1c
Ashley <a.clarkem@gmail.com>
2014-08-28 10:06:46 -0600

    bike_names returns bikes sold

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,12 @@
 class Shop < ActiveRecord::Base
   has_many :bikes
 
+  def bike_names
+    names = []
+    Bike.where(shop_id: self.id).find_each do |bike|
+      names.push(bike.name)
+    end
+    names
+  end
 
 end
