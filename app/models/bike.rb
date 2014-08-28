e1b0b731808cf2cad784f3149c05e8d8820af0e1
Ashley <a.clarkem@gmail.com>
2014-08-28 09:39:51 -0600

    can find buyer by favorite brand

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,4 +2,9 @@ class Bike < ActiveRecord::Base
 
   belongs_to :shop
   belongs_to :buyer
+
+  def favorited_by
+    buyer = Buyer.find_by(favorite_brand: self.brand)
+    "#{buyer.first_name}" + " #{buyer.last_name}"
+  end
 end
